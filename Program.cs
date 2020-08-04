bf6af1878ace2f88fe590e9b0a7fba7ff5b87606
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-05 01:25:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿/*using System;
+
+namespace ArrIndexMinMaxNumb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 15, 5, 19, 8, 20, 4, 13, 20, 65, 7, 8, 9, }; // 6 (4)
+
+            int min = arr[0];
+            int index = 0;
+ 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine("Индекс минимального элемента массива: " + index);
+            Console.ReadKey();
+        }
+    }
+}*/
+
+/*using System;
+
+namespace ArrIndexMinMaxNumb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 15, 5, 19, 8, 20, 4, 13, 20, 65, 7, 8, 9, }; // 9 (65)
+
+            int max = arr[0];
+            int index = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                    index = i;
+                }
+            }
+            Console.WriteLine("Индекс максимального элемента массива: " + index);
+            Console.ReadKey();
+        }
+    }
+}*/
